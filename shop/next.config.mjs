601b63508bed25a87c98951f72be399b16c04339
salif18 <salifmoctarkonate@gmail.com>
2024-11-22 00:07:50 +0000

    lpp

--- shop/next.config.mjs
+++ shop/next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-    appDir: true, // Active le App Router si ce n'est pas déjà fait
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
